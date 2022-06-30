ed0acd71f54df782509f2b65feb233718b2922c8
Skim1571 <skim1571@gmail.com>
2022-06-30 16:33:16 -0500

    completed all questions; saw  no bonus

--- script.js
+++ script.js
@@ -20,16 +20,34 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+const mainTitle = document.getElementById('main-title')
+mainTitle.innerText = "Dom's Home Page"
   // Part 2
-
+document.body.style.backgroundColor = "blue";
   // Part 3
-
+const favThings = document.getElementById('favorite-things').querySelectorAll('li');
+favThings[5].remove()
   // Part 4
+  const special = document.getElementsByClassName('special-title')
+  for (let i=0;i<special.length;i++){
+    special[i].style.fontSize = '2rem'
+  }
 
   // Part 5
-
+const pastRaces = document.getElementById('past-races').querySelectorAll('li');
+pastRaces[3].remove()
   // Part 6
-
+const newRace = document.createElement('li')
+newRace.innerText =' Seoul';
+document.getElementById('past-races').appendChild(newRace)
   // Part 7
+  const newBlog = document.createElement('div')
+  newBlog.setAttribute('class','blog-post purple')
+  document.querySelector('.main').appendChild(newBlog)
+  const newH1 = document.createElement('h1')
+  newH1.innerText = 'Seoul'
+  newBlog.appendChild(newH1)
+  const newP = document.createElement('p')
+  newP.innerText = "I yelled out, 'Ahn nyung!' as I sped through Gang-nam!"
+  newBlog.appendChild(newP)
 }
